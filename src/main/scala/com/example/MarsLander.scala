43300290d2787b22c748d2a28091960922c63cd5
smoothcriminal78 <dim.zaycev@gmail.com>
2016-08-22 08:20:07 +0300

    fix

--- src/main/scala/com/example/MarsLander.scala
+++ src/main/scala/com/example/MarsLander.scala
@@ -16,7 +16,8 @@ class MarsLander {
 
   def plotRoute(q: Queue[LandingData]): Queue[LandingData] = {
     val l = q.last
-    import l._
+    val e = LandingData(l.x,l.y,l.hs,l.vs,l.ang,l.th)
+    import e._
     if (y > maxY) {
       if (x < minX) {
         ang += (if (hs > 19 && ang > -90) 15 else calcRouteTrajectory(l))
@@ -24,7 +25,7 @@ class MarsLander {
         ang += (if (hs < -19 && ang > -30) -15 else if (hs > 19 && ang < 30) 15 else 0)
       }
       th += (if (vs > -39 && th < 4) 1 else if (vs < -39 && th > 0) -1 else 0)
-      q+=nextPoint(l)
+      q+=nextPoint(e)
       //level-off correction
 //      levelOff(q)
       plotRoute(q)
@@ -75,7 +76,7 @@ case class LandingData(var x: Int = 0, var y: Int = 0, var hs: Int = 0, var vs: 
 object MarsLander {
   def main(args: Array[String]): Unit = {
     val ml = new MarsLander
-    val q1 = ml.plotRoute(Queue(LandingData(2500, 2700)))
+    val q1 = ml.plotRoute(Queue(LandingData(2500,2700)))
     q1.foreach(println)
     val cgq = Queue(LandingData(2500, 2700, 0, 0, 0, 0, 0), LandingData(2500, 2699, 0, -3, -15, 1, 0), LandingData(2501, 2695, 1, -5, -30, 2, 0), LandingData(2503, 2689, 3, -6, -45, 3, 0), LandingData(2508, 2682, 7, -8, -60, 4, 0), LandingData(2517, 2674, 10, -9, -45, 4, 0), LandingData(2528, 2664, 13, -11, -60, 4, 0), LandingData(2543, 2653, 16, -12, -45, 4, 0), LandingData(2560, 2641, 19, -13, -60, 4, 0), LandingData(2581, 2627, 22, -14, -45, 4, 0), LandingData(2604, 2613, 24, -14, -30, 4, 0), LandingData(2629, 2598, 25, -14, -15, 4, 0), LandingData(2654, 2584, 25, -14, 0, 4, 0), LandingData(2679, 2571, 24, -14, 15, 4, 0), LandingData(2702, 2557, 22, -14, 30, 4, 0), LandingData(2723, 2542, 19, -15, 45, 4, 0), LandingData(2742, 2527, 17, -15, 30, 4, 0), LandingData(2759, 2512, 16, -15, 15, 4, 0), LandingData(2775, 2497, 16, -15, 0, 4, 0), LandingData(2792, 2483, 17, -15, -15, 4, 0), LandingData(2810, 2468, 19, -15, -30, 4, 0), LandingData(2831, 2453, 22, -16, -45, 4, 0), LandingData(2854, 2437, 24, -16, -30, 4, 0), LandingData(2879, 2421, 25, -16, -15, 4, 0), LandingData(2905, 2406, 25, -15, 0, 4, 0), LandingData(2929, 2390, 24, -15, 15, 4, 0))
     println("----------------------")
