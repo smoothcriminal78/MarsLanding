18da27f384896893f2d4c25fb73092f01b6196c9
smoothcriminal78 <dim.zaycev@gmail.com>
2016-08-26 01:27:14 +0300

    rangeAng

--- src/main/scala/com/example/MainWindow.scala
+++ src/main/scala/com/example/MainWindow.scala
@@ -16,9 +16,9 @@ import scala.util.Random
 object MainWindow extends SimpleSwingApplication {
   //easy on the right
   //val ground = List(0, 100, 1000, 500, 1500, 1500, 3000, 1000, 4000, 150, 5500, 150, 6999, 800)
-  
+
   //high ground
-  val ground = List(0,1000, 300,1500, 350,1400, 500,2100, 1500,2100, 2000,200, 2500,500, 2900,300, 3000,200, 3200,1000, 3500,500, 3800,800, 4000,200, 4200,800, 4800,600, 5000,1200, 5500,900, 6000,500, 6500,300, 6999,500)
+  val ground = List(0, 1000, 300, 1500, 350, 1400, 500, 2100, 1500, 2100, 2000, 200, 2500, 500, 2900, 300, 3000, 200, 3200, 1000, 3500, 500, 3800, 800, 4000, 200, 4200, 800, 4800, 600, 5000, 1200, 5500, 900, 6000, 500, 6500, 300, 6999, 500)
 
   var routes = Map[Int, List[LandingData]]()
 
@@ -39,11 +39,11 @@ object MainWindow extends SimpleSwingApplication {
 
         g.setColor(Color.RED)
         ground.map(_ / 10).sliding(4, 2).foreach(l => g.drawLine(l(0), size.height - l(1), l(2), size.height - l(3)))
-        routes.foreach {e =>
+        routes.foreach { e =>
           g.setColor(new Color(Random.nextFloat, Random.nextFloat, Random.nextFloat))
-          e._2.sliding(2).foreach(l => g.drawLine(l.head.x/10, size.height - l.head.y/10, l.last.x/10, size.height - l.last.y/10))
+          e._2.sliding(2).foreach(l => g.drawLine(l.head.x / 10, size.height - l.head.y / 10, l.last.x / 10, size.height - l.last.y / 10))
           val ld = e._2.last
-          g.drawString("x: %d, y: %d, hs: %d, vs %d, ang %d, th %d".format(ld.x, ld.y, ld.hs, ld.vs, ld.ang, ld.th), ld.x/10, size.height - ld.y/10)
+          g.drawString("x: %d, y: %d, hs: %d, vs %d, ang %d, th %d".format(ld.x, ld.y, ld.hs, ld.vs, ld.ang, ld.th), ld.x / 10, size.height - ld.y / 10)
         }
       }
     }
@@ -58,7 +58,7 @@ object MainWindow extends SimpleSwingApplication {
     reactions += {
       case ButtonClicked(component) if component == button => {
         val ml = new MarsLander
-        val r = ml.plotRoute(mutable.Queue(LandingData(6500, 2700, hs=50, ang=90))).flatMap(List(_)).toList
+        val r = ml.plotRoute(mutable.Queue(LandingData(6500, 2700, hs = -50, ang = 90))).flatMap(List(_)).toList
         //val r = ml.plotRoute(mutable.Queue(LandingData(7500, 2700))).flatMap(List(_)).toList
         routes += (if (routes.isEmpty) 0 else routes.keys.max + 1) -> r
 
--- src/main/scala/com/example/MarsLander.scala
+++ src/main/scala/com/example/MarsLander.scala
@@ -9,65 +9,85 @@ import scala.math._
 class MarsLander {
   val G = 3.711
 
-//  val p1 = 4000
-//  val p2 = 5500
-//  val alt = 150
+  //  val p1 = 4000
+  //  val p2 = 5500
+  //  val alt = 150
 
   val p1 = 500
   val p2 = 1500
   val alt = 2100
 
-  val segments = ArrayBuffer((0,1000), (300,1500), (350,1400), (500,2100), (1500,2100), (2000,200), (2500,500), (2900,300), (3000,200), (3200,1000), (3500,500), (3800,800), (4000,200), (4200,800), (4800,600), (5000,1200), (5500,900), (6000,500), (6500,300), (6999,500))
+  val segments = ArrayBuffer((0, 1000), (300, 1500), (350, 1400), (500, 2100), (1500, 2100), (2000, 200), (2500, 500), (2900, 300), (3000, 200), (3200, 1000), (3500, 500), (3800, 800), (4000, 200), (4200, 800), (4800, 600), (5000, 1200), (5500, 900), (6000, 500), (6500, 300), (6999, 500))
 
-  val ground = segments.sliding(2).filter{case(ArrayBuffer(v1,v2)) => v1._2==v2._2}.toList
-  val groundY = ground.map{case(ArrayBuffer(v1,v2))=>v1._2}.max
-  val groundX = ground.map{case(ArrayBuffer(v1,v2))=>(v1._1, v2._1)}.max
+  val ground = segments.sliding(2).filter { case (ArrayBuffer(v1, v2)) => v1._2 == v2._2 }.toList
+  val groundY = ground.map { case (ArrayBuffer(v1, v2)) => v1._2 }.max
+  val groundX = ground.map { case (ArrayBuffer(v1, v2)) => (v1._1, v2._1) }.max
 
   //val p1 = groundX._1
   //val p2 = groundX._2
   //val alt = groundY
 
+  /**
+    * Calculates shift in degrees - by 15 degrees max for turn
+    *
+    * @param a1 current angle
+    * @param a2 target angle
+    **/
+  def rangeAng(a1: Int, a2: Int) = {
+    val delta = if (a1 > 0 && a2 > 0) if (a1 > a2) a2 - a1 else a1 - a2
+    else if (a1 > 0 && a2 < 0) -1 * (a1 + abs(a2))
+    else if (a1 < 0 && a2 < 0) if (a1 > a2) -1 * (abs(a2) - abs(a1)) else abs(a2) - abs(a1)
+    else if (a1 < 0 && a2 > 0) abs(a1) + a2
+    else if (a1 == 0 && a2 > 0) -a2 else if (a1 == 0 && a2 < 0) a2 else if (a1 > 0 && a2 == 0) -a1 else if (a1 < 0 && a2 == 0) a1 else 0
+
+    delta match {
+      case d if d >= 15 => 15
+      case d if d <= -15 => -15
+      case d if (d > 0 && d < 15) || (d > -15 && d < 0) => d
+      case 0 => 0
+    }
+  }
+
   def plotRoute(q: Queue[LandingData]): Queue[LandingData] = {
     val e = q.last.copy()
     println(e)
+
     import e._
-      if (y > alt) {
-        ang += calcAngle(e)
-        //th = if (vs < -39) 4 else 3
-        th = if(ang>40 || vs < -39) 4 else 3
-        if(x<p1 || x>p2){
-          val ab = applyBraking(Queue(e.copy(t=2)), -ang)
-          if(ab._1.isEmpty) {
-            q += nextPoint(e)
-          } else {
-            q++=ab._1
-          }
-          plotRoute(q)
-          } else if(ang!=0) {
-          //level-off correction
-          val lo = levelOff(Queue(q.last.copy(t=1)))
-          q++=lo
-          plotRoute(q)
-        } else {
+
+    if (y > alt) {
+      ang += rangeAng(ang, calcAngle(e))
+      //th = if (vs < -39) 4 else 3
+      th = if (ang >= 22 || vs < -39) 4 else 3
+      if (x < p1 || x > p2) {
+        val ab = applyBraking(Queue(e.copy(t = 2)), -ang)
+        if (ab._1.isEmpty) {
           q += nextPoint(e)
-          plotRoute(q)
+        } else {
+          q ++= ab._1
         }
+        plotRoute(q)
+      } else if (ang != 0) {
+        //level-off correction
+        val lo = levelOff(Queue(q.last.copy(t = 1)))
+        q ++= lo
+        plotRoute(q)
       } else {
-        q
+        q += nextPoint(e)
+        plotRoute(q)
       }
+    } else {
+      q
     }
+  }
 
   def levelOff(q: Queue[LandingData]): Queue[LandingData] = {
     val e = q.last.copy()
-    e.ang += (
-      if (e.ang < 15) -e.ang
-      else if (e.ang >= 15) -15
-      else if (e.ang > -15) e.ang
-      else if (e.ang <= -15) 15
-      else 0
-      )
+    e.ang += rangeAng(e.ang, 0)
+
     val np = nextPoint(e)
+
     import np._
+
     q += np
     ang match {
       case 0 => if (x >= p1 || x <= p2 || y <= alt) q else Queue()
@@ -75,26 +95,28 @@ class MarsLander {
     }
   }
 
-  def applyBraking(q: Queue[LandingData], a:Int): (Queue[LandingData], Int) = {
+  def applyBraking(q: Queue[LandingData], a: Int): (Queue[LandingData], Int) = {
     val e = q.last.copy()
-    println(e)
-    e.ang=if(a>0) e.ang+15 else e.ang-15
-    if((a>0 && e.ang>a) || (a<0 && e.ang<a)) e.ang=a
+    e.ang = rangeAng(e.ang, -e.ang)
 
-    e.th = if(e.ang>40 || e.vs < -39) 4 else 3
+    e.th = if (e.ang >= 22 || e.vs < -39) 4 else 3
 
     val np = nextPoint(e)
+
     import np._
-    if(abs(hs)<=20 || np.hs>e.hs){
-      if(y>=alt && x>=p1 && x<=p2) ((q,a)) else ((Queue(), -1))
+
+    if (abs(hs) <= 20 || np.hs > e.hs || y <= alt) {
+      if (y >= alt && x >= p1 && x <= p2) ((q, a)) else ((Queue(), -1))
     } else {
       q += LandingData(x, y, hs, vs, ang, th, 2);
-      applyBraking(q,a)
+      applyBraking(q, a)
     }
   }
 
   def nextPoint(data: LandingData) = {
+
     import data._
+
     val incX = round(sin(toRadians(ang)) * th).toInt
     val incY = round(cos(toRadians(ang)) * th - G).toInt
 
@@ -102,20 +124,14 @@ class MarsLander {
   }
 
   def calcAngle(data: LandingData) = {
+
     import data._
-    val dx: Double = x - (if(x<p1) p1 else if(x>p2) p2 else p1+(p2-p1)/2)
+
+    val dx: Double = x - (if (x < p1) p1 else if (x > p2) p2 else p1 + (p2 - p1) / 2)
     val dy: Double = y - alt
 
-    val tarAng = round(toDegrees(atan(dy/dx))).toInt
-
-    val d = abs(tarAng) - abs(ang)
-
-     if (tarAng < ang) {
-       if(d >= 15) -15 else -tarAng
-     } else if (tarAng > ang) { 
-       if(d >=15) 15 else tarAng
-     }
-     else 0 
+    val a = round(toDegrees(atan(dx / dy))).toInt
+    if (a > 45) 22 else if (a < -45) -22 else a
   }
 }
 
@@ -124,7 +140,7 @@ case class LandingData(var x: Int = 0, var y: Int = 0, var hs: Int = 0, var vs: 
 object MarsLander {
   def main(args: Array[String]): Unit = {
     val ml = new MarsLander
-    val q1 = ml.plotRoute(Queue(LandingData(6500, 2700, hs=50, ang=90)))
+    val q1 = ml.plotRoute(Queue(LandingData(6500, 2700, hs = -50, ang = 90)))
     //val q1 = ml.plotRoute(Queue(LandingData(2500, 2700)))
     q1.foreach(println)
     //    println(ml.calcAngle(LandingData(4500, 2700)))
