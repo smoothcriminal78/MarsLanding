2fc849501a1cf4f0323be0585b70509fe7ddaac2
smoothcriminal78 <dim.zaycev@gmail.com>
2016-08-29 01:13:23 +0300

    fixed ranges

--- src/main/scala/com/example/MarsLander.scala
+++ src/main/scala/com/example/MarsLander.scala
@@ -65,14 +65,17 @@ class MarsLander {
       ang = rangeAng(ang, fa)
       th = rangeThr(th, calcThrottle(e))
       if (x < p1 || x > p2) {
-
-        val ga = if (ca > 0 && ca < 37) 36
-        else if (ca < 0 && ca > -37) -36
-        else if (ca >0 && ca < 40) 39
-        else if (ca < 0 && ca > -40) -39
-        else if (ca > 0 && ca >= 40) 22
-        else if (ca < 0 && ca <= -40) -22
-        else ca
+        println(s"ca $ca fa $fa")
+        val ga =
+        //          if (ca > 0 && ca < 37) 36
+        //        else if (ca < 0 && ca > -37) -36
+          if (ca > 0 && ca < 55) 36
+          else if (ca < 0 && ca > -55) -36
+          //        else if (ca >0 && ca < 40) 39
+          //        else if (ca < 0 && ca > -40) -39
+          else if (ca > 0 && ca >= 55) 22
+          else if (ca < 0 && ca <= -55) -22
+          else ca
 
         val ab = braking(Queue(e.copy(t = 2)), -ga)
         if (ab._1.isEmpty) {
@@ -166,24 +169,24 @@ case class LandingData(var x: Int = 0, var y: Int = 0, var hs: Int = 0, var vs: 
 
 object MarsLander {
   //easy on the right
-//    val segments = ArrayBuffer((0, 100), (1000, 500), (1500, 1500), (3000, 1000), (4000, 150), (5500, 150), (6999, 800))
-//    val init = Queue(LandingData(2500, 2700))
+      val segments = ArrayBuffer((0, 100), (1000, 500), (1500, 1500), (3000, 1000), (4000, 150), (5500, 150), (6999, 800))
+      val init = Queue(LandingData(2500, 2700))
 
   //initial speed, correct side
 //  val segments = ArrayBuffer((0, 100), (1000, 500), (1500, 100), (3000, 100), (3500, 500), (3700, 200), (5000, 1500), (5800, 300), (6000, 1000), (6999, 2000))
 //  val init = Queue(LandingData(6500, 2800, hs = -100, ang = 90))
 
   //initial speed, wrong side
-//                val segments = ArrayBuffer((0, 100), (1000, 500), (1500, 1500), (3000, 1000), (4000, 150), (5500, 150), (6999, 800))
-//                val init = Queue(LandingData(6500, 2800, hs = -90, ang = 90))
+//                  val segments = ArrayBuffer((0, 100), (1000, 500), (1500, 1500), (3000, 1000), (4000, 150), (5500, 150), (6999, 800))
+//                  val init = Queue(LandingData(6500, 2800, hs = -90, ang = 90))
 
   //Deep canyon
-        val segments = ArrayBuffer((0, 1000), (300, 1500), (350, 1400), (500, 2000), (800, 1800), (1000, 2500), (1200, 2100), (1500, 2400), (2000, 1000), (2200, 500), (2500, 100), (2900, 800), (3000, 500), (3200, 1000), (3500, 2000), (3800, 800), (4000, 200), (5000, 200), (5500, 1500), (6999, 2800))
-        val init = Queue(LandingData(500, 2700, hs = 100, ang = -90))
+//          val segments = ArrayBuffer((0, 1000), (300, 1500), (350, 1400), (500, 2000), (800, 1800), (1000, 2500), (1200, 2100), (1500, 2400), (2000, 1000), (2200, 500), (2500, 100), (2900, 800), (3000, 500), (3200, 1000), (3500, 2000), (3800, 800), (4000, 200), (5000, 200), (5500, 1500), (6999, 2800))
+//          val init = Queue(LandingData(500, 2700, hs = 100, ang = -90))
 
   //high ground
-  //  val segments = ArrayBuffer((0, 1000), (300, 1500), (350, 1400), (500, 2100), (1500, 2100), (2000, 200), (2500, 500), (2900, 300), (3000, 200), (3200, 1000), (3500, 500), (3800, 800), (4000, 200), (4200, 800), (4800, 600), (5000, 1200), (5500, 900), (6000, 500), (6500, 300), (6999, 500))
-  //  val init = Queue(LandingData(6500, 2700, hs = -50, ang = 90))
+//      val segments = ArrayBuffer((0, 1000), (300, 1500), (350, 1400), (500, 2100), (1500, 2100), (2000, 200), (2500, 500), (2900, 300), (3000, 200), (3200, 1000), (3500, 500), (3800, 800), (4000, 200), (4200, 800), (4800, 600), (5000, 1200), (5500, 900), (6000, 500), (6500, 300), (6999, 500))
+//      val init = Queue(LandingData(6500, 2700, hs = -50, ang = 90))
 
   val landSpot = segments.sliding(2).filter { case (ArrayBuffer(v1, v2)) => v1._2 == v2._2 }.toList
   val landSpotY = landSpot.map { case (ArrayBuffer(v1, v2)) => v1._2 }.max
