0cac7ddf2013f2c1d0c04bb6ab0efd8a8aef3db8
smoothcriminal78 <dim.zaycev@gmail.com>
2016-09-02 08:56:07 +0300

    fixed ranges

--- src/main/scala/com/example/MarsLander.scala
+++ src/main/scala/com/example/MarsLander.scala
@@ -48,89 +48,38 @@ class MarsLander {
     if (th1 > th2 && th1 > 0) th1 - 1 else if (th1 < th2 && th1 < 4) th1 + 1 else th1
   }
 
-  def plotRoute(q: Queue[LandingData]): Queue[LandingData] = {
+  def plotRoute(q: List[LandingData]): List[LandingData] = {
     val e = q.last.copy()
-    println(e)
-
-    import e._
-
+    e.ang = rangeAng(e.ang, angle(e))
+    e.th = rangeThr(e.th, throttle(e))
+    val np = nextPoint(e)
+    import np._
     if (y > alt) {
-      val ca = calcAngle(e)
-      val fa = if (x >= p1 && x <= p2 && ang == 0) 0
-      else if (ca > 39) 22
-      else if (ca < -39) -22
-      else ca
-
-
-      ang = rangeAng(ang, fa)
-      th = rangeThr(th, calcThrottle(e))
-      if (x < p1 || x > p2) {
-        println(s"ca $ca fa $fa")
-        val ga =
-        //          if (ca > 0 && ca < 37) 36
-        //        else if (ca < 0 && ca > -37) -36
-          if (ca > 0 && ca < 55) 36
-          else if (ca < 0 && ca > -55) -36
-          //        else if (ca >0 && ca < 40) 39
-          //        else if (ca < 0 && ca > -40) -39
-          else if (ca > 0 && ca >= 55) 22
-          else if (ca < 0 && ca <= -55) -22
-          else ca
-
-        val ab = braking(Queue(e.copy(t = 2)), -ga)
-        if (ab._1.isEmpty) {
-          q += nextPoint(e)
-        } else {
-          q ++= ab._1
-        }
-        plotRoute(q)
-      } else if (ang != 0) {
-        //level-off correction
-        val lo = levelOff(Queue(q.last.copy(t = 1)))
-        q ++= lo
-        plotRoute(q)
+      if (x < p1 || x > p2 || abs(hs) > 10) {
+        // braking
+        val ga = (if (hs < 0) -1 else 1) * (if (abs(e.ang) < 22) 45 else if (abs(e.ang) < 55) 36 else 22)
+        val bq = braking(List(e.copy(ang = ga, t = 1)))
+        if(bq.nonEmpty) plotRoute(q ++ bq) else plotRoute(q :+ np)
+      } else if (x >= p1 && x <= p2 && (ang != 0 || hs != 0)) {
+        //leveloff
+        e.ang = if (e.hs > 0) rangeAng(e.ang, 15) else if (e.hs < 0) rangeAng(e.ang, -15) else rangeAng(e.ang, 0)
+        plotRoute(q :+ np)
       } else {
-        q += nextPoint(e)
-        plotRoute(q)
+        plotRoute(q :+ np)
       }
-    } else {
-      q
-    }
+    } else q
   }
 
-  def levelOff(q: Queue[LandingData]): Queue[LandingData] = {
+  def braking(q: List[LandingData]): List[LandingData] = {
     val e = q.last.copy()
-    //println(s"leveloff: $e")
-    val na = if (e.hs > 0) 15 else if (e.hs < 0) -15 else 0
-    e.ang = rangeAng(e.ang, na)
-
+    e.th = throttle(e)
     val np = nextPoint(e)
 
     import np._
-
-    q += np
-    ang match {
-      case 0 => if (x >= p1 || x <= p2 || y <= alt) q.tail else Queue()
-      case _ => levelOff(q)
+    if (hs == 0 || e.x == np.x || y <= alt || x < 0) {
+      if (y >= alt && x >= p1 && x <= p2) q.tail else List()
     }
-  }
-
-  def braking(q: Queue[LandingData], a: Int): (Queue[LandingData], Int) = {
-    val e = q.last.copy()
-    println(s"braking -------------------------------------------> $e")
-    e.ang = rangeAng(e.ang, a)
-    e.th = rangeThr(e.th, calcThrottle(e))
-    val np = nextPoint(e)
-
-    import np._
-    q += LandingData(x, y, hs, vs, ang, th, 2)
-    //    (hs <= 20 && x >= p1 && x + hs <= p2) || (hs >= -20 && hs < 0 && x <= p2 && x + hs >= p1) ||
-    if (hs == 0 || e.x == np.x || e.x == np.x + np.hs || y <= alt || x < 0) {
-      //      println(s"braking -------------------------------------------> $e")
-      //      println(s"braking -------------------------------------------> $np")
-      if (y >= alt && x >= p1 && x <= p2) (q.tail, a) else (Queue(), -1)
-    }
-    else braking(q, a)
+    else braking(q :+ np)
   }
 
   def nextPoint(data: LandingData) = {
@@ -143,25 +92,26 @@ class MarsLander {
     LandingData(x + hs - incX / 2, y + vs + incY / 2, hs - incX, vs + incY, ang, th, t)
   }
 
-  def calcAngle(data: LandingData) = {
+  def angle(data: LandingData) = {
 
     import data._
 
-    val dx: Double = x - (if (x < p1) p1 else if (x > p2) p2 else p1 + (p2 - p1) / 2)
+    val dx: Double = x - mid
     val dy: Double = y - alt
 
-    round(toDegrees(atan(dx / dy))).toInt
+    (if (abs(hs) > 40) -1 else 1) * (round(toDegrees(atan(dx / dy))).toInt match {
+      case a if ang == 0 && x >= p1 && x <= p2 => 0
+      case a if a > 39 => 22
+      case a if a < -39 => -22
+      case a => a
+    })
   }
 
-  def calcThrottle(data: LandingData) = {
+  def throttle(data: LandingData) = {
+
     import data._
-    //    if (ang <= -22 || ang >= 22 || vs < -39) 4 else if (ang < 22 || ang > -22) 3 else 2
-    if (vs < -39) 4
-    else if (t == 2 && ((hs < 0 && ang < 0) || (hs > 0 && ang > 0))) 4
-    //    else if (t == 1 && vs> -39) 2
-    else if (ang == 0) 4
-    else if (ang <= -22 || ang >= 22) 4
-    else 3
+
+    if (vs > 39 && ang == 0) 3 else 4
   }
 }
 
@@ -169,32 +119,26 @@ case class LandingData(var x: Int = 0, var y: Int = 0, var hs: Int = 0, var vs: 
 
 object MarsLander {
   //easy on the right
-      val segments = ArrayBuffer((0, 100), (1000, 500), (1500, 1500), (3000, 1000), (4000, 150), (5500, 150), (6999, 800))
-      val init = Queue(LandingData(2500, 2700))
+  val segments = ArrayBuffer((0, 100), (1000, 500), (1500, 1500), (3000, 1000), (4000, 150), (5500, 150), (6999, 800))
+  val init = List(LandingData(2500, 2700))
 
   //initial speed, correct side
-//  val segments = ArrayBuffer((0, 100), (1000, 500), (1500, 100), (3000, 100), (3500, 500), (3700, 200), (5000, 1500), (5800, 300), (6000, 1000), (6999, 2000))
-//  val init = Queue(LandingData(6500, 2800, hs = -100, ang = 90))
+  //  val segments = ArrayBuffer((0, 100), (1000, 500), (1500, 100), (3000, 100), (3500, 500), (3700, 200), (5000, 1500), (5800, 300), (6000, 1000), (6999, 2000))
+  //  val init = Queue(LandingData(6500, 2800, hs = -100, ang = 90))
 
   //initial speed, wrong side
-//                  val segments = ArrayBuffer((0, 100), (1000, 500), (1500, 1500), (3000, 1000), (4000, 150), (5500, 150), (6999, 800))
-//                  val init = Queue(LandingData(6500, 2800, hs = -90, ang = 90))
+  //                  val segments = ArrayBuffer((0, 100), (1000, 500), (1500, 1500), (3000, 1000), (4000, 150), (5500, 150), (6999, 800))
+  //                  val init = Queue(LandingData(6500, 2800, hs = -90, ang = 90))
 
   //Deep canyon
-//          val segments = ArrayBuffer((0, 1000), (300, 1500), (350, 1400), (500, 2000), (800, 1800), (1000, 2500), (1200, 2100), (1500, 2400), (2000, 1000), (2200, 500), (2500, 100), (2900, 800), (3000, 500), (3200, 1000), (3500, 2000), (3800, 800), (4000, 200), (5000, 200), (5500, 1500), (6999, 2800))
-//          val init = Queue(LandingData(500, 2700, hs = 100, ang = -90))
+  //          val segments = ArrayBuffer((0, 1000), (300, 1500), (350, 1400), (500, 2000), (800, 1800), (1000, 2500), (1200, 2100), (1500, 2400), (2000, 1000), (2200, 500), (2500, 100), (2900, 800), (3000, 500), (3200, 1000), (3500, 2000), (3800, 800), (4000, 200), (5000, 200), (5500, 1500), (6999, 2800))
+  //          val init = Queue(LandingData(500, 2700, hs = 100, ang = -90))
 
   //high ground
-//      val segments = ArrayBuffer((0, 1000), (300, 1500), (350, 1400), (500, 2100), (1500, 2100), (2000, 200), (2500, 500), (2900, 300), (3000, 200), (3200, 1000), (3500, 500), (3800, 800), (4000, 200), (4200, 800), (4800, 600), (5000, 1200), (5500, 900), (6000, 500), (6500, 300), (6999, 500))
-//      val init = Queue(LandingData(6500, 2700, hs = -50, ang = 90))
+  //      val segments = ArrayBuffer((0, 1000), (300, 1500), (350, 1400), (500, 2100), (1500, 2100), (2000, 200), (2500, 500), (2900, 300), (3000, 200), (3200, 1000), (3500, 500), (3800, 800), (4000, 200), (4200, 800), (4800, 600), (5000, 1200), (5500, 900), (6000, 500), (6500, 300), (6999, 500))
+  //      val init = Queue(LandingData(6500, 2700, hs = -50, ang = 90))
 
-  val landSpot = segments.sliding(2).filter { case (ArrayBuffer(v1, v2)) => v1._2 == v2._2 }.toList
-  val landSpotY = landSpot.map { case (ArrayBuffer(v1, v2)) => v1._2 }.max
-  val landSpotX = landSpot.map { case (ArrayBuffer(v1, v2)) => (v1._1, v2._1) }.max
-
-  val p1 = landSpotX._1
-  val p2 = landSpotX._2
-  val alt = landSpotY
+  val (alt, p1, p2, mid) = segments.sliding(2).filter { case Seq(v1, v2) => v1._2 == v2._2 }.map { case Seq(v1, v2) => (v1._2, v1._1, v2._1, v1._1 + (v2._1 - v1._1) / 2) }.max
 
   def main(args: Array[String]): Unit = {
     println(s"p1 $p1 p2 $p2 alt $alt")
